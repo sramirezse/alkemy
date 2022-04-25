9c3add8916fbdeee7876b4caf91dfd86da392a6c
Gerardo Ramirez <sramirezse@gmail.com>
2022-04-25 11:59:01 -0500

    cambios en footer

--- resources/views/home/layout/footer.blade.php
+++ resources/views/home/layout/footer.blade.php
@@ -1,12 +1,12 @@
-<footer class="row">
-    <div class="col-lg-6">
+<footer style="" class="row">
+    <div class="col-lg-6 col-md-12" style="">
         <b>
             <p>Derechos reservados</p>
             <p>La reproduccion completa o parcial de este material está prohibida</p>
             <p>&#169; 2020 Grupo de Compañías GSK. NP-xxxx-xxx Agosto 2020</p>
         </b>
     </div>
-    <div class="col-lg-6 container pb-2">
+    <div class="col-lg-6 col-md-12 container pb-2">
         <div class="row">
             <div class="col-4"> <img width="50" src="images/icon_twitter.png" alt=""> </div>
             <div class="col-4"> <img width="50" src="images/icon_youtube.png" alt=""> </div>
