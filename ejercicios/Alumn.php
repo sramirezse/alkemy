afb75a011d73a3c5a15ac5cbbcc20fe34f2d4cad
Gerardo Ramirez <sramirezse@gmail.com>
2022-04-26 13:08:43 -0500

    norm

--- ejercicios/Alumn.php
+++ ejercicios/Alumn.php
@@ -10,12 +10,12 @@ class Alumn extends Person {
     protected $nombre = 'Gerardo';
     protected $genero = 'Hombre';
     protected $edad = '26';
-    protected $Cumpleaños = '1995-08-02';
-    public function __construct($nombre, $genero, $edad, $cumpleaños) {
+    protected $cumpleanios = '1995-08-02';
+    public function __construct($nombre, $genero, $edad, $cumpleanios) {
         $this->nombre = $nombre;
         $this->genero = $genero;
         $this->edad = $edad;
-        $this->cumpleaños = $cumpleaños;
+        $this->cumpleanios = $cumpleanios;
     }
     public function getNombre(){
         return $this->nombre;
@@ -26,7 +26,7 @@ class Alumn extends Person {
     public function getEdad(){
         return $this->edad;
     }
-    public function getCumpleaños(){
+    public function getCumpleanios(){
         return $this->Cumpleaños;
     }
 
@@ -39,7 +39,7 @@ class Alumn extends Person {
     public function setEdad($edad){
         $this->edad = $edad;
     }
-    public function setCumpleaños($Cumpleaños){
+    public function setCumpleanios($Cumpleaños){
         $this->Cumpleaños = $Cumpleaños;
     }
 
