cb4d86129cc9c49499bc12e719af60e7c4ba8ce4
Gerardo Ramirez <sramirezse@gmail.com>
2022-04-26 23:19:50 -0500

    refactorizacion del controlador de section, en el commit pasado se a;adio un middleware para los roles, si es un usuario comun retorna un error de role al intentar eliminar una entrada

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use App\Models\SectionContent;
 use Illuminate\Support\Facades\Storage;
+
 class SectionController extends Controller
 {
     /**
@@ -16,143 +17,68 @@ class SectionController extends Controller
      */
     public function index()
     {
-        //
     }
 
-    protected function getSectionContent(Request $request){
-
-    }
-    public function uploadImagesToBanner(Request $request)
+    protected function getSectionContent(Request $request)
     {
         $section = Section::find($request->section)->with('contents')->first();
         $content = $section->contents->first();
         $content = SectionContent::find($content->id);
+        return $content;
+    }
+    
+    public function saveContent(SectionContent $content, $images)
+    {
+        $contentd = [
+            'images' => $images
+        ];
+        $content->content = $contentd;
+        $content->save();
+        return $content;
+    }
+
+    public function fetchImagesToBanner(Request $request)
+    {
+        $content = $this->getSectionContent($request);
+        $images = $content->content['images'];
+        return $images;
+    }
+
+    public function uploadImagesToBanner(Request $request)
+    {
+        $content = $this->getSectionContent($request);
         $ext = $request->file('file')->getClientOriginalExtension();
-        $name = 'section_' . $section->id . '_' . $request->prefix . '_image_' . time() . '.' . $ext;
-        // dd($name);
+        $name = 'section_' . $content->section_id . '_' . $request->prefix . '_image_' . time() . '.' . $ext;
         $images = $content->content['images'];
         $image = $request->file->move(public_path('images'), $name);
-        // dd($request->all());
         $image = [
             'alt' => $request->alt,
             'path' => 'images/' . $name,
         ];
 
         array_push($images, $image);
-        $contentd = [
-            'images' => $images
-        ];
-        $content->content = $contentd;
-        $content->save();
+        return $content = $this->saveContent($content, $images);
+
     }
     public function updateBanner(Request $request)
     {
-
-        // dd($request->all());
-        $section = Section::find($request->section)->with('contents')->first();
-        $content = $section->contents->first();
-        $content = SectionContent::find($content->id);
+        $content = $this->getSectionContent($request);
         $images = $content->content['images'];
-        // dd($request->all());
         $image = [
             'alt' => $request->alt,
             'path' => $request->path,
         ];
-        // dd($image);
         $images[$request->index] = $image;
-        $contentd = [
-            'images' => $images
-        ];
-        $content->content = $contentd;
-        $content->save();
+        return $content = $this->saveContent($content, $images);
+
     }
     public function deleteBanner(Request $request)
     {
-        $section = Section::find($request->section)->with('contents')->first();
-        $content = $section->contents->first();
-        $content = SectionContent::find($content->id);
+        $content = $this->getSectionContent($request);
         $images = $content->content['images'];
         unset($images[$request->index]);
-        $contentd = [
-            'images' => $images
-        ];
-        $content->content = $contentd;
-        $content->save();
+        return $content = $this->saveContent($content, $images);
     }
 
-    public function fetchImagesToBanner(Request $request)
-    {
-        $section = Section::find($request->section)->with('contents')->first();
-        $content = $section->contents->first();
-        $content = SectionContent::find($content->id);
-        $images = $content->content['images'];
-        // $images = arsort($images);
-        // dd($images);
-        return $images;
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Section  $section
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Section $section)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Section  $section
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Section $section)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Section  $section
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Section $section)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Section  $section
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Section $section)
-    {
-        //
-    }
+    
 }
